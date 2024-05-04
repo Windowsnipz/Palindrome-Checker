9a99ac65f279724607b3b9ead9225aae6cd9925c
windowsnipz <nstrickland2014@gmail.com>
2024-05-04 17:24:20 -0400

    More logical progress on the js functions. Also updated the button to submit uppon hitting the enter key

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ const inputText = document.getElementById('text-input');
 const checkButton = document.getElementById('check-btn');
 
 
-checkButton.addEventListener('click', () => {
-    alert(`You typed: ${inputText.value}`);
-});
+checkButton.addEventListener('click', checkWord);
 
 
 
 function checkWord() {
-
-};
+    if (!isValidInput()) {
+        alert('Please input a value')
+    } else {
+        alert('Value entered! Good job!')
+    }
+}
 
 function isValidInput() {
-
-};
+    if (inputText.value == "") {
+        return false;
+    }
+    return true;
+}
