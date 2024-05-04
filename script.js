68a61c24f815b4766b9fc2306f16557c3b6479f3
windowsnipz <nstrickland2014@gmail.com>
2024-05-04 17:10:37 -0400

    playing around with event listeners

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const inputText = document.getElementById('text-input');
 const checkButton = document.getElementById('check-btn');
 
 
-
+checkButton.addEventListener('click', () => {
+    alert(`You typed: ${inputText.value}`);
+});
 
 
 
